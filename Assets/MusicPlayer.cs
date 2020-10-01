b828c962579148cb5dd1877b7d04c3fd62d70966
Max <maxanderberg@gmail.com>
2020-10-01 08:04:27 +0200

    Init music player

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MusicPlayer : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadFirstLevel",3f);
+    }
+
+
+    void LoadFirstLevel(){
+        SceneManager.LoadScene(1);
+    }
+
+
+}
