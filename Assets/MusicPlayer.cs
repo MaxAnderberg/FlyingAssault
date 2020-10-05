ee378f5314636bb3c953347a02719a258e528c13
Max <maxanderberg@gmail.com>
2020-10-05 06:04:56 +0200

    made the music player a singleton

--- Assets/MusicPlayer.cs
+++ Assets/MusicPlayer.cs
@@ -6,10 +6,26 @@ using UnityEngine.SceneManagement;
 
 public class MusicPlayer : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+
+    void Awake()
+    {
+        GameObject[] objs = GameObject.FindGameObjectsWithTag("music");
+
+        if (objs.Length > 1)
+        {
+            Destroy(this.gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+
+// Start is called before the first frame update
+void Start()
     {
         Invoke("LoadFirstLevel",3f);
+        
     }
 
 
