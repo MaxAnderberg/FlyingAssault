3a77765a0e1c7ffd380fff0a2faa54f7adf48bfc
Max <maxanderberg@gmail.com>
2020-10-05 22:10:11 +0200

    calculating x offset

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -1,10 +1,12 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
 
 
 public class Player : MonoBehaviour
 {
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10f;
     // Start is called before the first frame update
     void Start()
     {
@@ -14,6 +16,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+
+        float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        float xOffset = xThrow * xSpeed * Time.deltaTime;
+
+
+
+
     }
 }
