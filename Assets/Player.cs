c387450c87280273500c24be2e0435d90a49e13f
Max <maxanderberg@gmail.com>
2020-10-06 19:00:22 +0200

    Updated player script. Calculating raw x pos and then clamping it so we can move our player left and right

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -7,6 +7,7 @@ using UnityStandardAssets.CrossPlatformInput;
 public class Player : MonoBehaviour
 {
     [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10f;
+    [Tooltip("In ms")] [SerializeField] float xRange = 5f;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +20,9 @@ public class Player : MonoBehaviour
 
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
-
-
-
+        float rawNewXPos = transform.localPosition.x + xOffset;
+        float xPos = Mathf.Clamp(rawNewXPos,-xRange, xRange);
+        transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
 
     }
 }
