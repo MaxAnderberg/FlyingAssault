8abbf73cc877f567e5bed3ef3437bfe60e09d848
Max <maxanderberg@gmail.com>
2020-10-08 07:35:09 +0200

    Updated player script - can now move vertically as well as horizontally - using the same speed value for both axis (xSpeed)

--- Assets/Player.cs
+++ Assets/Player.cs
@@ -6,8 +6,12 @@ using UnityStandardAssets.CrossPlatformInput;
 
 public class Player : MonoBehaviour
 {
-    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 10f;
+    [Tooltip("In ms^-1")] [SerializeField] float xSpeed = 15f;
+    [Tooltip("In ms^-1")] [SerializeField] float ySpeed = 15f; // atm I'm not using this. Only using the xSpeed for both vertically and horizontal movement
+
     [Tooltip("In ms")] [SerializeField] float xRange = 5f;
+    [Tooltip("In ms")] [SerializeField] float yRange = 5f;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -18,11 +22,20 @@ public class Player : MonoBehaviour
     void Update()
     {
 
+        // vertical
+        float yTthrow = CrossPlatformInputManager.GetAxis("Vertical");
+        float yOffset = yTthrow * xSpeed * Time.deltaTime;
+        float rawNewYPos = transform.localPosition.y + yOffset;
+        float yPos = Mathf.Clamp(rawNewYPos, -yRange, yRange);
+
+        // horizontal 
         float xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
         float xOffset = xThrow * xSpeed * Time.deltaTime;
         float rawNewXPos = transform.localPosition.x + xOffset;
         float xPos = Mathf.Clamp(rawNewXPos,-xRange, xRange);
-        transform.localPosition = new Vector3(xPos, transform.localPosition.y, transform.localPosition.z);
+        transform.localPosition = new Vector3(xPos, yPos, transform.localPosition.z);
+
+
 
     }
 }
