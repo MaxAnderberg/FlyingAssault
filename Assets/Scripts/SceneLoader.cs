f34419a6de2d18ada11de3abe0a85dfbfd7f86cf
Max <maxanderberg@gmail.com>
2020-10-22 07:18:18 +0200

    Removed the update method as we don't need it

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,11 +1,10 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class SceneLoader : MonoBehaviour
 {
-
-
     // Start is called before the first frame update
     void Start()
     {
@@ -15,11 +14,5 @@ public class SceneLoader : MonoBehaviour
     {
         SceneManager.LoadScene(1);
     }
-}
 
-// Update is called once per frame
-void Update()
-    {
-        
-    }
 }
