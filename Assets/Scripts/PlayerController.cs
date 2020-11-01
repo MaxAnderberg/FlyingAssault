201de75fd8add598143b425b0e1af7f825ab9bca
Max <maxanderberg@gmail.com>
2020-11-01 20:16:42 +0100

    Added a toggle for particle emission

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -73,27 +73,20 @@ public class PlayerController : MonoBehaviour
     {
         if (CrossPlatformInputManager.GetButton("Fire")) 
         {
-            ActivateGuns();
+            SetGunsActive(true);
         }
         else
         {
-            DeactivateGuns();
+            SetGunsActive(false);
         }
     }
 
-    private void ActivateGuns()
+    private void SetGunsActive(bool isActive)
     {
         foreach (GameObject gun in guns)
         {
-            gun.SetActive(true);
-        }
-    }
-
-    private void DeactivateGuns()
-    {
-        foreach (GameObject gun in guns)
-        {
-            gun.SetActive(false);
+            var gunParticleEmssion = gun.GetComponent<ParticleSystem>().emission;
+            gunParticleEmssion.enabled = isActive;
         }
     }
 }
